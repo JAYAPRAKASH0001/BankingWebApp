13a4c70b2daf61370b106a83b235010d442134a3
dhanush-0212 <dhanush.dec01@gmail.com>
2025-02-08 11:51:03 +0530

    finished all modules

---
+++ src/main/java/com/bank/bankingapp/Exceptionhandler/ExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.bank.bankingapp.Exceptionhandler;
+
+public class ExceptionHandler extends RuntimeException {
+    public ExceptionHandler(String message) {
+
+        super(message);
+    }
+}
